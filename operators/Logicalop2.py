ff93ca8e46d2274e6d68836155cf5e5d86e44b5e
samirvithlani <samir.vithlani83955@gmail.com>
2026-04-04 19:12:29 +0530

    and or

---
+++ operators/Logicalop2.py
@@ -0,0 +1,8 @@
+tickt = True
+online = False
+offline =False
+
+if tickt and (online or offline):
+    print("you can travel..")
+else:
+    print("you can not travel...")    
---
+++ operators/logicalop.py
@@ -0,0 +1,7 @@
+age =19
+voterId = False
+
+if age>=18 and voterId:
+    print("you can vote")
+else:
+    print("you can not vote.")    
