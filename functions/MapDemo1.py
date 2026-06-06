f040926688eeba44961351503ab74f0f3a245a33
samirvithlani <samir.vithlani83955@gmail.com>
2026-06-06 17:23:22 +0530

    map

---
+++ functions/MapDemo1.py
@@ -0,0 +1,42 @@
+marks =[10,20,30,40,50,60]
+updatedmarks =[]
+
+for i in marks:
+    updatedmarks.append(i+10)
+
+print(updatedmarks)    
+
+#compr
+updatedmarks2 = [i+1 for i in marks]    
+print(updatedmarks2)
+
+#map
+updatedmarks3 = map(lambda x:x+10,marks)
+print(list(updatedmarks3))
+
+data = ("amit","sumit","raj","parth")
+upperData = map(lambda x:x.upper(),data)
+print(tuple(upperData))
+
+upperData2 = map(lambda x:x.startswith("s"),data)
+print(list(upperData2))
+
+
+
+sales = [11,22,123,45,66,78,90,98,97,13]
+salesLb = map(lambda x:"even" if x % 2==0 else "odd",sales)
+print(list(salesLb))
+
+
+data = ("amit","sumit","raj","parth")
+datalen = list(map(lambda x:len(x),data))
+print(datalen)
+
+data = ("amit","sumit","naman","raj","bob","parth")
+palindromenames = list(map(lambda x:"palindrome" if x == x[::-1] else "not",data))
+print(palindromenames)
+
+data = [121,23,33,678,99,100,111]
+
+palindromenums = list(map(lambda x:"palindrome" if str(x) ==str(x)[::-1] else "not",data))
+print(palindromenums)
