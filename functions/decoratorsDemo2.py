91db87395949a496bb97ae717ad79c84eff80e53
samirvithlani <samir.vithlani83955@gmail.com>
2026-06-10 19:29:24 +0530

    param decorators

---
+++ functions/decoratorsDemo2.py
@@ -0,0 +1,20 @@
+
+def smartdiv(func): #func == div
+    
+    def inner(x,y):
+        print("innner called..",x,y)
+        if(y==0):
+            print("can not divide by zero :")
+        else:
+            func(x,y)    
+    
+    return inner        
+    
+
+@smartdiv
+def div(no1,no2):
+    print("div called ",no1/no2)
+
+
+div(10,0)    
+    
